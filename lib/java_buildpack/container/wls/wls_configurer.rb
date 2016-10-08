6338b7c631e9572bea31e6212e580d2caf3eb426
Ben Hale <bhale@pivotal.io>
2016-10-07 17:20:39 -0700

    Additional Logging
    Previously when there was some sort of failure, an error message was printed
    encouraging the review of a log file.  That log file was then immediately
    recycled as part of container destruction preventing anything useful being
    gleaned from it.  This change actually prints the log file when there is a
    failure.

--- lib/java_buildpack/container/wls/wls_configurer.rb
+++ lib/java_buildpack/container/wls/wls_configurer.rb
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'java_buildpack/logging/logger_factory'
+
 module JavaBuildpack
   module Container
     module Wls
@@ -23,6 +25,7 @@ module JavaBuildpack
         include JavaBuildpack::Container::Wls::WlsConstants
 
         def initialize(configuration_map)
+          @logger                   = JavaBuildpack::Logging::LoggerFactory.instance.get_logger WlsConfigurer
           @app_name                 = configuration_map['app_name']
           @application              = configuration_map['application']
           @app_services_config      = configuration_map['app_services_config']
@@ -56,11 +59,14 @@ module JavaBuildpack
           unless wls_home_glob
             log_and_print("Problem with install, can't locate weblogic.jar, check captured install log output " \
                           "at #{@wls_install}/install.log")
+
+            @logger.info { File.read("#{wls_install}/install.log") }
           end
 
           @wls_home = File.dirname(wls_home_glob) + '/../..'
           unless @wls_home
             log_and_print("Problem with install, check captured install log output at #{@wls_install}/install.log")
+            @logger.info { File.read("#{wls_install}/install.log") }
           end
 
           log("WebLogic install is located at : #{@wls_install}")
